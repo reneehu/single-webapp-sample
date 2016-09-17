3e38e02e2e8c37d7150e276e862004aa17408f6f
reneehu <reneehu217@gmail.com>
2016-09-17 17:37:10 -0400

    string.charCodeAt calculate

--- app.js
+++ app.js
@@ -4,11 +4,25 @@
 	angular.module('myApp',[])
 
 	.controller('myCtlr', function($scope){
-		$scope.name = "hi";
-		$scope.sayhello = function(){
-			return "Hello Course";
+		$scope.name = "";
+		$scope.totalNum = 0;
+		$scope.displayNum = function(){
+			var totalNameNum = caculateNumForString($scope.name);
+			$scope.totalNum = totalNameNum;
 		};
+
+		function caculateNumForString(string){
+			var totalStringValue = 0;
+			for(var i=0;i<string.length;i++){
+				totalStringValue += string.charCodeAt(i);
+			}
+			return totalStringValue;
+		}
+
 	});
+
+
+
 
 
 })();
