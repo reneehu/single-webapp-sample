50563533596b5df4a73ae878c6677adfe797aa45
reneehu <reneehu217@gmail.com>
2016-09-19 00:28:47 -0400

    green/red

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -9,6 +9,7 @@
 	function LunchCheckController($scope){
 		$scope.lunchMessage = "I don't know yet";
 		$scope.food = null;
+		$scope.validMsg = false;
 		$scope.displayMsg = function(){
 			var checkMsg = checkFood($scope.food);
 			$scope.lunchMessage = checkMsg;
@@ -19,10 +20,11 @@
 				return "Please enter data first";
 
 			var foodArg = foodString.split(',');
-			if(foodArg.length>0 && foodArg.length<4){
-				return "Enjoy!";
+			$scope.validMsg = true;
+			if(foodArg.length<4){	
+				return "Enjoy!";	
 			}else {
-				return "Too much!";
+				return "Too much!";	
 			}
 
 		};
